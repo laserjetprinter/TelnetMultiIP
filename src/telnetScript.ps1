34040fc710c217d813adb2da8bc1dd44a62f8815
Natalie Davis <Natalie.Davis@blackbaud.me>
2018-07-18 15:15:14 -0400

    1st commit

---
+++ src/telnetScript.ps1
@@ -0,0 +1,17 @@
+﻿$path = "C:\Users\natalie.davis\Documents\TelnetScriptLogs\ExampleIPs.csv"
+$csv = Import-csv -path $path
+$python="C:\Users\natalie.davis\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Python 3.7 (32-bit).lnk"
+$PythonScript="C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLogSort.py"
+
+Import-Csv $path | Foreach-Object { 
+
+    foreach ($property in $_.PSObject.Properties)
+    {
+        $property.Value >>C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLog.csv 
+        New-Object System.Net.Sockets.TcpClient($property.Value , 3246) >>C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLog.csv 
+    } 
+
+}
+
+ 
+& $python $PythonScript
