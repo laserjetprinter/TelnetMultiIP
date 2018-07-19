233460ed7c3e35825f83d179bbabfca77aca343e
Natalie Davis <Natalie.Davis@blackbaud.me>
2018-07-19 11:46:50 -0400

    filepath correction

--- src/telnetScript.ps1
+++ src/telnetScript.ps1
@@ -1,14 +1,14 @@
-﻿$path = "C:\Users\natalie.davis\Documents\TelnetScriptLogs\ExampleIPs.csv"
+﻿$path = "EXAMPLE-IP-CSV-FILEPATH"
 $csv = Import-csv -path $path
-$python="C:\Users\natalie.davis\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Python 3.7 (32-bit).lnk"
-$PythonScript="C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLogSort.py"
+$python="PYTHON-EXE-FILEPATH"
+$PythonScript="PYTHON-TELNETLOGSORT-FILEPATH"
 
 Import-Csv $path | Foreach-Object { 
 
     foreach ($property in $_.PSObject.Properties)
     {
-        $property.Value >>C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLog.csv 
-        New-Object System.Net.Sockets.TcpClient($property.Value , 3246) >>C:\Users\natalie.davis\Documents\TelnetScriptLogs\telnetLog.csv 
+        $property.Value >>TELNET-LOG-FILEPATH
+        New-Object System.Net.Sockets.TcpClient($property.Value , 80) >>TELNET-LOG-FILEPATH
     } 
 
 }
